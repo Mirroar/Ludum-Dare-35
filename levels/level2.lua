7cb6cd9d2cdf233b1e25899da3c68906d6e7f3e2
David Franke <mirroar@gmail.com>
2016-04-17 12:55:10 +0200

    made level 2 an actual puzzle

--- levels/level2.lua
+++ levels/level2.lua
@@ -3,52 +3,57 @@ levels.level2 = Level2
 
 local mapStrings = {
     map = {
-        '   ....',
-        '  .....',
-        ' ....##',
-        '.......',
-        '....## ',
-        '.....  ',
-        '....   ',
+        '      ###',
+        '     #..#',
+        '    #...#',
+        '    #.## ',
+        '  ##...##',
+        ' #.#.....',
+        '#.#..#...',
+        '#..##... ',
+        '### #..  ',
     },
     entities = {
-        '       ',
-        '       ',
-        '   OO  ',
-        '      x',
-        '  OO   ',
-        '       ',
-        '       ',
+        '         ',
+        '         ',
+        '         ',
+        '     +   ',
+        '         ',
+        '     o+oo',
+        ' * +     ',
+        ' Oo   O  ',
+        '      x  ',
     },
 }
 
 function Level2:construct()
-    Level.construct(self, 7, 7)
+    Level.construct(self, 9, 9)
 
-    self.mapOffset.x = -16
+    self.mapOffset.x = -32
     self.mapOffset.y = 50
 
     -- Initialize map.
     self:LoadMap(mapStrings)
 
-    -- Add a small door.
-    entities:GetTile(5, 4):SetType('smalldoor', {
-        w = true,
-        e = true,
-    })
-    entities:GetTile(6, 4):SetType('smalldoor', {
-        w = true,
-        e = true,
-    })
+    -- Add doors.
+    entities:GetTile(4, 7).exits.ne = true
+    entities:GetTile(4, 7).exits.sw = true
+    entities:GetTile(6, 4).exits.nw = true
+    entities:GetTile(6, 4).exits.se = true
+
+    entities:GetTile(2, 7).callbacks.press = function()
+        entities:GetTile(7, 6).exits.e = true
+        entities:GetTile(7, 6).exits.w = true
+    end
 
     -- Add a snake.
-    snake = Snake(map, {{4, 1}, {5, 1}, {6, 1}})
+    snake = Snake(map, {{7, 2}, {8, 2}, {8, 3}, {7, 3}})
 end
 
 -- Gets called when an entity is eaten, for the level to respond.
 function Level2:EntityEaten(entityType, x, y)
     if entityType == 'bigfood' and not self.sentText then
-        log:insert("Eating red pellets will also make you fat. You will not fit through tight spaces this way.")
+        log:insert("After eating a red pellet, snake will not fit through tight spaces.")
         log:insert("You may press 'R' to retry the current level.")
         self.sentText = true
     end
