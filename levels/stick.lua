dbfad24c13bf36ced048af54cac15b2f65b28e3a
David Franke <mirroar@gmail.com>
2016-04-18 21:52:59 +0200

    Added functionality to stick level

--- levels/stick.lua
+++ levels/stick.lua
@@ -3,29 +3,29 @@ levels.stick = StickLevel
 
 local mapStrings = {
     map = {
-        '                 ',
-        '                 ',
-        '                 ',
-        '                 ',
-        '                 ',
-        '.................',
-        '                 ',
-        '                 ',
-        '                 ',
-        '                 ',
-        '                 ',
+        '   ####   ###    ',
+        '  #...#  #..#    ',
+        ' #..### #.#.#    ',
+        '###...# ##.#     ',
+        '#.#.#####..######',
+        '#...............#',
+        '#######.###.###.#',
+        '     #.## #...###',
+        '    #.#.# #.#..# ',
+        '    #..#  #...#  ',
+        '    ###   ####   ',
     },
     entities = {
         '                 ',
-        '                 ',
-        '                 ',
-        '                 ',
-        '                 ',
-        '                x',
-        '                 ',
-        '                 ',
-        '                 ',
-        '                 ',
+        '    +*           ',
+        '  o        O     ',
+        '   ++o    o      ',
+        ' o               ',
+        ' +         +  o+ ',
+        '       +       o ',
+        '           +x    ',
+        '       *   +  o  ',
+        '             o   ',
         '                 ',
     },
 }
@@ -39,6 +39,43 @@ function StickLevel:construct()
     -- Initialize map.
     self:LoadMap(mapStrings)
 
+    -- Add exits to doors.
+    entities:GetTile(2, 6).exits.e = true
+    entities:GetTile(2, 6).exits.nw = true
+
+    entities:GetTile(5, 2).exits.e = true
+    entities:GetTile(5, 2).exits.w = true
+
+    entities:GetTile(12, 6).exits.e = true
+    entities:GetTile(12, 6).exits.w = true
+
+    entities:GetTile(8, 7).exits.ne = true
+    entities:GetTile(8, 7).exits.nw = true
+    entities:GetTile(8, 7).exits.sw = true
+
+    entities:GetTile(4, 4).exits.nw = true
+    entities:GetTile(4, 4).exits.e = true
+    entities:GetTile(4, 4).exits.se = true
+
+    -- Add button functionality.
+    entities:GetTile(6, 2).callbacks.press = function()
+        entities:GetTile(5, 4).exits.e = true
+        entities:GetTile(5, 4).exits.w = true
+
+        entities:GetTile(12, 6).exits.se = true
+    end
+
+    entities:GetTile(8, 9).callbacks.press = function()
+        entities:GetTile(16, 6).exits.w = true
+        entities:GetTile(16, 6).exits.se = true
+
+        entities:GetTile(12, 8).exits.nw = true
+        entities:GetTile(12, 8).exits.se = true
+
+        entities:GetTile(12, 9).exits.nw = true
+        entities:GetTile(12, 9).exits.se = true
+    end
+
     -- Add a snake.
-    snake = Snake(map, {{1, 6}, {2, 6}, {3, 6}})
+    snake = Snake(map, {{10, 3}, {11, 2}})
 end
